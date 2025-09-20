027a5ea21b10d3530fd6c40e6946f02c6694a13b
sha1kh4 <adnan.shaikh166@mit.asia>
2025-09-20 16:30:38 +0530

    navbar to be sticky

---
+++ fronted/src/app/admin/page.js
@@ -0,0 +1,16 @@
+import { NavigationMenuDemo } from "@/components/navbar"
+
+export default function Page() {
+  return (
+    <>
+      <div className="sticky top-0 bg-white z-50 w-full max-w-sm mx-auto">
+        <NavigationMenuDemo />
+      </div>
+
+      {/* Rest of your page content */}
+      <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+        {/* Your other content here */}
+      </div>
+    </>
+  )
+}
